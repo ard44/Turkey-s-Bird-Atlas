a72463a3cbd01b93e97d262c97da582f2d84ca51
ard44 <ardatastanpl@gmail.com>
2026-06-08 05:01:40 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,48 +1,55 @@
 from flask import Flask, render_template, request, redirect
+import sqlite3
 
 app = Flask(__name__)
 
 
-observations = []
-obs_id_counter = 1
-
-
-import sqlite3
 def get_db_connection():
     conn = sqlite3.connect('bird_atlas.db')
     conn.row_factory = sqlite3.Row
     return conn
 
+
+def clear_db():
+    conn = get_db_connection()
+    conn.execute('DELETE FROM observations')
+    conn.commit()
+    conn.close()
+
+
+clear_db() 
+
 @app.route('/')
 def index():
     conn = get_db_connection()
     birds = conn.execute('SELECT * FROM species').fetchall()
+    
+    
+    obs = conn.execute('''
+        SELECT observations.*, species.name_tr 
+        FROM observations 
+        JOIN species ON observations.bird_id = species.id
+    ''').fetchall()
+    
     conn.close()
-    return render_template('index.html', birds=birds, observations=observations)
+    return render_template('index.html', birds=birds, observations=obs)
 
 @app.route('/add_observation', methods=['POST'])
 def add_observation():
-    global obs_id_counter
     bird_id = request.form['bird_id']
     note = request.form['note']
-    
-    
     conn = get_db_connection()
-    bird = conn.execute('SELECT name_tr FROM species WHERE id = ?', (bird_id,)).fetchone()
+    conn.execute('INSERT INTO observations (bird_id, note) VALUES (?, ?)', (bird_id, note))
+    conn.commit()
     conn.close()
-    
-    observations.append({
-        'id': obs_id_counter,
-        'name_tr': bird['name_tr'],
-        'note': note
-    })
-    obs_id_counter += 1
     return redirect('/')
 
 @app.route('/delete/<int:id>')
 def delete(id):
-    global observations
-    observations = [ob for ob in observations if ob['id'] != id]
+    conn = get_db_connection()
+    conn.execute('DELETE FROM observations WHERE id = ?', (id,))
+    conn.commit()
+    conn.close()
     return redirect('/')
 
 if __name__ == '__main__':
